70b12367c1acf1bd9c5df15b1b8a41866325e10e
elugabriel <gabrielelufidodo2016@gmail.com>
2022-04-08 04:13:00 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ *  * main - prints all numbers of base 16.
+ *   *
+ *    * Return: 0 on success
+ *     */
+
+int main(void)
+{
+		char letter;
+
+			for (letter = '0'; letter <= '9'; letter++)
+					{
+								putchar(letter);
+
+									}
+				for (letter = 'a'; letter <= 'f'; letter++)
+						{
+									putchar(letter);
+										}
+					putchar('\n');
+
+						return (0);
+}
+
